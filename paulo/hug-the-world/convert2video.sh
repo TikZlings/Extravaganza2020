a43e351ba8c54db231a5abe19a2c2d447c4717f2
samcarter <sam.carter8@aol.com>
2020-11-25 14:03:56 +0100

    converting hug the world to video

---
+++ paulo/hug-the-world/convert2video.sh
@@ -0,0 +1,18 @@
+# clean up old video
+rm hug-the-world.mp4
+
+# convert to png images
+convert -density 240 hug-the-world.pdf hug-the-world.png
+
+# convert to video
+ffmpeg -ss 00:00:00 -i hug-the-world-%d.png -ss 00:00:00 -i ./assets/hug-the-world-modified.mp3 -shortest hug-the-world_raw.mp4
+
+# repair video
+HandBrakeCLI --crop 0:0:0:0  -i hug-the-world_raw.mp4 -o hug-the-world.mp4
+
+# clean up
+rm hug-the-world-*.png
+rm hug-the-world_raw.mp4
+
+# view :)
+open hug-the-world.mp4
