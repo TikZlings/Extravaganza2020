88ab35b30be602b33b5e0b1ecb528bc8877067da
samcarter <sam.carter8@aol.com>
2020-12-14 21:59:47 +0100

    arg, latexmk think the pdf is up-to-date ; switching back to normal latex for now

--- intermissions/convert2video.sh
+++ intermissions/convert2video.sh
@@ -3,13 +3,16 @@
 convert2video(){
 
     # compiling
-    latexmk -jobname=extravaganza-intermission-$1 -pretex="\def\mytitle{$2}" -usepretex extravaganza-intermission
+    # latexmk -jobname=extravaganza-intermission-$1 -pretex="\def\mytitle{$2}" -usepretex extravaganza-intermission
+
+    pdflatex --jobname=extravaganza-intermission-$1 "\def\mytitle{"$2"}\input{extravaganza-intermission}"
+    pdflatex --jobname=extravaganza-intermission-$1 "\def\mytitle{"$2"}\input{extravaganza-intermission}"
     
     # clean up old video
     rm $1.mp4
 
     # convert to png images
-    convert -density 160 $1-repeat.pdf $1.png
+    convert -density 160 extravaganza-intermission-$1.pdf $1.png
 
     # convert to video
     ffmpeg -r 10 -ss 00:00:00 -i $1-%d.png -ss 00:00:04 -i JingleBells.m4a -shortest $1_raw.mp4
@@ -17,19 +20,20 @@ convert2video(){
     # repair video
     HandBrakeCLI --crop 0:0:0:0 -i $1_raw.mp4 -o $1.mp4
 
+    # clean up
+    rm $1-*.png
+    rm $1_raw.mp4
+    rm extravaganza-intermission-$1.aux
+    rm extravaganza-intermission-$1.log
+    rm extravaganza-intermission-$1.nav
+    rm extravaganza-intermission-$1.snm
+    rm extravaganza-intermission-$1.toc
+    rm extravaganza-intermission-$1.out
+
 }
 
 # Converting
 convert2video venice "A night in venice"
+convert2video baaaa "Baaaaaaaaa"
 
-# clean up
-rm *.aux
-rm *.log
-rm *.nav
-rm *.snm
-rm *.toc
-rm *.out
-rm *.png
-rm *_raw.mp4
-rm *.fdb_latexmk
-rm *.fls
+
