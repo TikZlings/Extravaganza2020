1fd9c230f9382150209dcca3d9def191369ac1ee
samcarter <sam.carter8@aol.com>
2020-12-15 17:42:40 +0100

    preparations for intermission videos

--- intermissions/convert2video.sh
+++ intermissions/convert2video.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# passing a title to the intermission
+# Params:
+# 1: jobname
+# 2: some string for the title
 convert2video(){
 
     # compiling
@@ -31,6 +35,42 @@ convert2video(){
 
 }
 
+# repeat an existing pdf several times
+# Params:
+# 1: jobname
+# 2: path and name to the pdf file
+# 3: number of repetitions [default 50]
+repeatconvert(){
+
+    # compiling
+    latexmk -jobname=extravaganza-intermission-$1 -pretex="\def\filename{$2}\def\myrepeat{$3}" -usepretex repeat  
+    
+    # clean up old video
+    rm $1.mp4
+
+    # convert to png images
+    convert -density 160 extravaganza-intermission-$1.pdf $1.png
+
+    # convert to video
+    ffmpeg -r 10 -ss 00:00:00 -i $1-%d.png -ss 00:00:04 -i JingleBells.m4a -shortest $1_raw.mp4
+
+    # repair video
+    HandBrakeCLI --crop 0:0:0:0 -i $1_raw.mp4 -o $1.mp4
+
+    # clean up
+    rm $1-*.png
+    rm $1_raw.mp4
+    rm extravaganza-intermission-$1.aux
+    rm extravaganza-intermission-$1.log
+    rm extravaganza-intermission-$1.nav
+    rm extravaganza-intermission-$1.snm
+    rm extravaganza-intermission-$1.toc
+    rm extravaganza-intermission-$1.out
+    rm extravaganza-intermission-$1.fls
+    rm extravaganza-intermission-$1.fdb_latexmk
+        
+}
+
 # Converting
 convert2video bobbles "Christmas Outing"
 convert2video bear "Bär Necessity"
@@ -39,10 +79,14 @@ convert2video lochness "Amazing Ducks"
 convert2video venice "A Night in Venice"
 convert2video hug "Hug the World"
 convert2video arts "Tik\textit{Z}Art, yet again"
-#convert2video bees "Two Bees \\ or \\ Not Two Bees \par \normalsize\raggedleft Thanks to Marmot! \tikz[scale=0.3]\marmot;"
+convert2video bees "Two Bees \linebreak or \linebreak Not Two Bees \par \normalsize\hfill \makebox[2.8cm][l]{Thanks to Marmot! \tikz[scale=0.3]\marmot;}"
 convert2video downunder "Down Under"
 convert2video zoom "Modern Times"
 convert2video xcoffins "For Insiders Only"
 convert2video queen "The Magic Flute"
 convert2video vampire "Sinister"
 convert2video finale "Grand Finale"
+
+
+repeatconvert title "../ulrike/intermission/extravaganza-titlepage.pdf" 225
+repeatconvert credits "../ulrike/intermission/extravaganza-2020-credits.pdf" 150
--- videos/collectvideos.sh
+++ videos/collectvideos.sh
@@ -1,7 +1,7 @@
 COUNTER=1
 
-# title
-cp ../intermissions/bobble.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
+cp ../intermissions/title.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
+cp ../intermissions/bobbles.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../ulrike/bobble/bobble.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;    
 cp ../intermissions/bear.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../ulrike/honey/honey.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
@@ -15,7 +15,7 @@ cp ../intermissions/hug.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../paulo/hug-the-world/hug-the-world.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../intermissions/arts.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../samcarter/art/art.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
-#cp ../intermissions/.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
+cp ../intermissions/bees.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../samcarter/bees/bees.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../intermissions/downunder.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../ulrike/downunder/downunder.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
@@ -29,4 +29,4 @@ cp ../intermissions/vampire.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../samcarter/vampires/vampires.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;   
 cp ../intermissions/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
 cp ../ulrike/finale/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;   
-# credits
+cp ../intermissions/credits.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;  
